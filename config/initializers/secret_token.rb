3c019ff575cce87bcbfb06b729979702240ebbc5
Dorian Karter <dkarter@iit.edu>
2012-10-28 05:08:47 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InfoMoto::Application.config.secret_token = '1af160b70d3e963c2114b8885249226bce9fde45c7c9f4af7b72fb3d49e51bfa7629852068a30f7b7ab667a8e2ee285b68528540fd99981941e4556c6f285706'
